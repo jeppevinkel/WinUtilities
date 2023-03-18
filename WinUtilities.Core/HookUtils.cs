29d1ffe55532e82b599f0bdabf95a70b5c9ef348
Jeppe Beier <jeppevinkel@gmail.com>
2023-03-18 22:38:09 +0100

    Windows message hooks.

---
+++ WinUtilities.Core/HookUtils.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WinUtilities.Core
+{
+    public static class HookUtils
+    {
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc,
+            WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+        
+        /// <summary>
+        /// Installs an application-defined hook procedure into a hook chain.
+        /// </summary>
+        /// <param name="idHook">The type of hook procedure to be installed.</param>
+        /// <param name="lpfn">A pointer to the hook procedure. If the dwThreadId parameter is zero or specifies the identifier of a thread created by a different process, the lpfn parameter must point to a hook procedure in a DLL. Otherwise, lpfn can point to a hook procedure in the code associated with the current process.</param>
+        /// <param name="hMod">A handle to the DLL containing the hook procedure pointed to by the lpfn parameter.</param>
+        /// <param name="dwThreadId">The identifier of the thread with which the hook procedure is to be associated. For desktop apps, if this parameter is zero, the hook procedure is associated with all existing threads running in the same desktop as the calling thread.</param>
+        /// <returns>If the function succeeds, the return value is the handle to the hook procedure.</returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr SetWindowsHookEx(int idHook,
+            WindowsHookExDelegate lpfn, IntPtr hMod, uint dwThreadId);
+        
+        /// <summary>
+        /// Removes a hook procedure installed in a hook chain by the SetWindowsHookEx function.
+        /// </summary>
+        /// <param name="hhk">A handle to the hook to be removed. This parameter is a hook handle obtained by a previous call to SetWindowsHookEx.</param>
+        /// <returns>If the function succeeds, the return value is nonzero.</returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnhookWindowsHookEx(IntPtr hhk);
+        
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode,
+            IntPtr wParam, IntPtr lParam);
+        
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
+    }
+    
+    public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild,
+        uint dwEventThread, uint dwmsEventTime);
+        
+    public delegate IntPtr WindowsHookExDelegate(
+        int nCode, IntPtr wParam, IntPtr lParam);
+}
---
+++ WinUtilities.Core/MessageUtils.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace WinUtilities.Core
+{
+    public static class MessageUtils
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct POINT
+        {
+            public int X;
+            public int Y;
+
+            public POINT(int x, int y)
+            {
+                this.X = x;
+                this.Y = y;
+            }
+
+            public static implicit operator System.Drawing.Point(POINT p)
+            {
+                return new System.Drawing.Point(p.X, p.Y);
+            }
+
+            public static implicit operator POINT(System.Drawing.Point p)
+            {
+                return new POINT(p.X, p.Y);
+            }
+
+            public override string ToString()
+            {
+                return $"X: {X}, Y: {Y}";
+            }
+        }
+        
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MSG
+        {
+            IntPtr hwnd;
+            uint message;
+            UIntPtr wParam;
+            IntPtr lParam;
+            int time;
+            POINT pt;
+            int lPrivate;
+        }
+
+        [DllImport("user32.dll")]
+        public static extern int GetMessage(out MSG lpMsg, IntPtr hWnd, uint wMsgFilterMin,
+            uint wMsgFilterMax);
+
+        [DllImport("user32.dll")]
+        public static extern bool TranslateMessage([In] ref MSG lpMsg);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr DispatchMessage([In] ref MSG lpmsg);
+
+        /// <summary>
+        /// This starts a blocking loop that receives windows messages. This can be used for programs that don't subscribe to the windows message loop by default, like console applications.
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        public static void StartMessageLoop(CancellationToken? cancellationToken = null)
+        {
+            Console.WriteLine("Start");
+            // Console.WriteLine(GetMessage(out var msg, HookUtils.GetModuleHandle(curModule?.ModuleName), 0, 0));
+            MSG msg;
+            int ret;
+            Console.WriteLine(GetMessage(out msg, IntPtr.Zero, 0, 0));
+
+            // while ((ret = GetMessage(out var msg, IntPtr.Zero, 0, 0)) != 0)
+            // {
+            //     return;
+            //     if (ret == -1)
+            //     {
+            //         Console.WriteLine("Something is wrong!");
+            //         return;
+            //     }
+            //     
+            //     
+            //     TranslateMessage(ref msg);
+            //     DispatchMessage(ref msg);
+            // }
+            Console.WriteLine("Stop");
+        }
+    }
+}
